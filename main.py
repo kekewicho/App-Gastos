af5a17847083087a7069b6173e412e8b98cdf2a8
Luis Ramos <113364516+kekewicho@users.noreply.github.com>
2022-12-28 09:30:53 -0600

    Ingre y egre funcionando correctamente

---
+++ main.py
@@ -0,0 +1,109 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton as bt
+from kivy.uix.screenmanager import ScreenManager,Screen
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.list import TwoLineListItem
+from kivy.animation import Animation
+import datetime
+import crud
+
+balance=0
+actual=''
+
+class Content(MDBoxLayout):
+	pass
+
+class Scr(ScreenManager):
+	def validacion(self,field,text):
+		if field=='monto':
+			return text.isnumeric()
+		if field=='descripcion':
+			if not text=='':return True
+			else: False
+	
+	def trasladar_quincena(self,direction):
+		global actual,balance
+		balance=0
+		a=crud.quince_actual('valor',direction,actual)
+		self.ids.quincena.text=crud.translate_codigo(a)
+		actual=a
+		for operation in ('ingresos','egresos'):
+			self.ids[operation].clear_widgets()
+			data=crud.db.child('ingre_egre').child(actual).child(operation).get()
+			if data.each() is None:
+				pass
+			else:
+				for value in data.each():
+					if operation=='ingresos':
+						balance+=eval(value.val()['monto'])
+						self.ids.ingresos.add_widget(TwoLineListItem(text='$'+value.val()['monto'],secondary_text=value.val()['descripcion']))
+					elif operation=='egresos':
+						balance-=eval(value.val()['monto'])
+						self.ids.egresos.add_widget(TwoLineListItem(text='$'+value.val()['monto'],secondary_text=value.val()['descripcion']))
+		self.ids.balance.text="Balance: ${:,.2f}".format(balance)
+
+
+	def registro(self,operation):
+		def clean_fields():
+			self.dialog.content_cls.ids.monto.text=''
+			self.dialog.content_cls.ids.descripcion.text=''
+		
+		def registrar(object):
+			global balance, actual
+			monto=self.dialog.content_cls.ids.monto.text
+			descripcion=self.dialog.content_cls.ids.descripcion.text
+			quincena=self.ids.quincena.text
+			if self.validacion('monto',monto) and self.validacion('descripcion',descripcion):
+				crud.db.child('ingre_egre').child(actual).child(operation).push({'monto':monto,'descripcion':descripcion})
+				self.ids[operation].add_widget(TwoLineListItem(text='$'+self.dialog.content_cls.ids.monto.text,secondary_text=self.dialog.content_cls.ids.descripcion.text))
+				clean_fields()
+				if operation=='ingresos': balance+=eval(monto)
+				elif operation=='egresos': balance-=eval(monto)
+				self.ids.balance.text='Balance: '+"${:,.2f}".format(balance)
+				self.dialog.dismiss()
+			else:
+				self.dialog.content_cls.ids.monto.error=True
+				self.dialog.content_cls.ids.descripcion.error=True
+		
+		def cancelar(object):
+			clean_fields()
+			self.dialog.dismiss()
+		
+		self.dialog=MDDialog(
+				title='Registrar '+operation,
+				type='custom',
+				content_cls=Content(),
+				buttons=[
+					bt(text='Cancelar',on_release=cancelar),
+					bt(text='Registrar',on_release=registrar)]
+			)
+		self.dialog.open()
+
+Builder.load_file('main.kv')
+
+class Appson(MDApp):
+	def build(self):
+		return Scr()
+	
+	def on_start(self):
+		global balance
+		global actual
+		actual=crud.quince_actual('new')
+		quincena_actual=crud.translate_codigo(actual)
+		self.root.ids.quincena.text=quincena_actual
+		for operation in ('ingresos','egresos'):
+			data=crud.db.child('ingre_egre').child(actual).child(operation).get()
+
+			for value in data.each():
+				if operation=='ingresos':
+					balance+=eval(value.val()['monto'])
+					self.root.ids.ingresos.add_widget(TwoLineListItem(text='$'+value.val()['monto'],secondary_text=value.val()['descripcion']))
+				elif operation=='egresos':
+					balance-=eval(value.val()['monto'])
+					self.root.ids.egresos.add_widget(TwoLineListItem(text='$'+value.val()['monto'],secondary_text=value.val()['descripcion']))
+		self.root.ids.balance.text+="${:,.2f}".format(balance)
+		
+if __name__=="__main__":
+	Appson().run()
