370f88be4f2ed7de5ef2a09a1f3faf8063a43dcc
kekewicho <luisruben2997@gmail.com>
2023-04-13 11:14:17 -0600

    Funcionamiento de gastos concubinos completo

--- widgets/widgets.py
+++ widgets/widgets.py
@@ -2,9 +2,11 @@ from kivy.lang import Builder
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.card import MDCard, MDCardSwipe
 from kivymd.uix.pickers import MDDatePicker
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty,NumericProperty
 from kivymd.app import MDApp
 from datetime import date
+from kivy.animation import Animation
+from kivy.utils import platform
 
 class GastoConcubinoItem(MDCard):
     quien=StringProperty()
@@ -20,13 +22,43 @@ class GastoConcubinoItem(MDCard):
         self.key=key
 
 class ConcubinosContent(MDBoxLayout):
+    who = 'luis' if platform != 'macosx' else 'joss'
+    quien=StringProperty(who)
     def __init__(self,*args):
         super().__init__(*args)
         self.ids.fecha.text=str(date.today())
+        for chip in self.ids.boxChip.children:
+            chip.bind(active=self.set_chip_bg_color)
+            chip.bind(active=self.set_chip_text_color)
+
+    def set_chip_bg_color(self, instance_chip, active_value: int):
+        '''
+        Will be called every time the chip is activated/deactivated.
+        Sets the background color of the chip.
+        '''
+        print(instance_chip, active_value)
+        instance_chip.md_bg_color = (
+            (0, 0, 0, 0.4)
+            if active_value
+            else (
+                instance_chip.theme_cls.bg_darkest
+                if instance_chip.theme_cls.theme_style == "Light"
+                else (
+                    instance_chip.theme_cls.bg_light
+                    if not instance_chip.disabled
+                    else instance_chip.theme_cls.disabled_hint_text_color
+                )
+            )
+        )
+
+    def set_chip_text_color(self, instance_chip, active_value: int):
+        Animation(
+            color=(0, 0, 0, 1) if active_value else (0, 0, 0, 0.5), d=0.2
+        ).start(instance_chip.ids.label)
 
     def get_data(self):
         data = {
-            'quien': self.ids.quien.text,
+            'quien': (self.quien).capitalize(),
             'cantidad': float(self.ids.cantidad.text),
             'fecha':self.ids.fecha.text
         }
@@ -34,7 +66,6 @@ class ConcubinosContent(MDBoxLayout):
     
     def clean_fields(self):
         self.ids.fecha.text=str(date.today())
-        self.ids.quien.text=''
         self.ids.cantidad.text=''
 
     def show_calendar(self):
